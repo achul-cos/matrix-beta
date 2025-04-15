63e76f9cb8b7c06184b263f42755a508595fdc93
achul <waduhgaming554@gmail.com>
2025-04-15 16:30:22 +0700

    achul coba coba mngerjain home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Matrix</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="flex flex-col p-4 space-y-4">
+        <div class="mt-4 bg-white rounded-xl shadow-xl overflow-hidden transition-transform transform hover:scale-101">
+            <div class="p-8 grid grid-cols-2 gap-4">
+                <div class="justify-start">
+                    <h3 class="text-3xl font-bold">Halo, <span class="text-lime-600">Achul</span> </h3>
+                    <p class="text-lg text-gray-500">Hari ini sewa komputer apa?</p>
+                </div>
+                <div class="grid grid-cols-2 flex justify-end">
+                    <div class="w-1/5 h-1/5"><img src="https://placehold.co/100" alt="" class="shadow-md rounded-lg"></div>    
+                </div>    
+            </div>
+        </div>
+    </div>
+</body>
+</html>
